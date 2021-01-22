1cc031c59a09e777526803c231f8e5ad499465c4
itguny <the2133@naver.com>
2021-01-22 23:00:03 +0900

    '2021-1-22-23:0'

--- auto.py
+++ auto.py
@@ -2,7 +2,7 @@
 import os
 import schedule
 import time
-
+import winsound
 
 # function
 
@@ -22,7 +22,9 @@ def git_init():
     os.system("git branch -M main")
     os.system("git push -u origin main")
 
-    return 
+    winsound.Beep(200,100)
+
+    return
 
     # commit
 def auto_commit():
@@ -30,27 +32,31 @@ def auto_commit():
 
     os.system("git add -A")
     print("git add -A")
-    os.system(f"git commit -m '{this_tm.tm_year}-{this_tm.tm_mon}-{this_tm.tm_mday}-{this_tm.tm_hour}-{this_tm.tm_min}'")
-    print(f"git commit -m '{this_tm.tm_year}-{this_tm.tm_mon}-{this_tm.tm_mday}-{this_tm.tm_hour}-{this_tm.tm_min}'")
+    os.system(f"git commit -m '{this_tm.tm_year}-{this_tm.tm_mon}-{this_tm.tm_mday}-{this_tm.tm_hour}:{this_tm.tm_min}'")
+    print(f"git commit -m '{this_tm.tm_year}-{this_tm.tm_mon}-{this_tm.tm_mday}-{this_tm.tm_hour}:{this_tm.tm_min}'")
     os.system("git push")
     print("git push")
 
+    winsound.Beep(200,100)
+
+    return 
 
 # schedule
 schedule.every().day.at("12:00").do(auto_commit)
 schedule.every().day.at("17:00").do(auto_commit)
-schedule.every().day.at("21:55").do(auto_commit)
 schedule.every().day.at("23:00").do(auto_commit)
 
 # Main
-
 if __name__  == '__main__':
 
     if 'Y' == input('You need Init? Y or N (default: N): '):
         git_init()
 
+    for i in range(4):
+        winsound.Beep(150,300)
+
     print('!!! running !!!')
-    
+
     while True:
         schedule.run_pending()
         time.sleep(1)
